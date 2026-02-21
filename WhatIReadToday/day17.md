a845affc3ac7114a1981eb26ffb954f971a49dbc
Deepanshu Chaurasia <deepanshustm2003@gmail.com>
2026-02-21 22:48:30 +0530

    day 17 learnings

---
+++ WhatIReadToday/day17.md
@@ -0,0 +1,5 @@
+today i started again recursion
+solved 1 one basic question how to find subset of given string
+
+Also, some improvement in bank management project.
+ I add FastCash page today.
---
+++ src/com/deepu/dsa/recursion/SubSeq.java
@@ -0,0 +1,20 @@
+package com.deepu.dsa.recursion;
+
+public class SubSeq {
+    static void main() {
+        subseq("","abc");
+    }
+
+    static void subseq(String p, String up){
+        if(up.isEmpty()){
+            System.out.println(p);
+            return;
+        }
+
+        char ch = up.charAt(0);
+
+        subseq(p+ch,up.substring(1));
+        subseq(p,up.substring(1));
+    }
+
+}
