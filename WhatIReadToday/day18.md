f9a97f158f6c09b16ecb973d576c5e843d4fa22f
Deepanshu Chaurasia <deepanshustm2003@gmail.com>
2026-02-24 05:00:56 +0530

    day 18 learnings

---
+++ WhatIReadToday/day18.md
@@ -0,0 +1,4 @@
+
+understand the core concept of recursion and solved problem of how to find alll the subset of given string
+
+Currently my exam is going on, so that's it for today
--- src/com/deepu/dsa/recursion/SubSeq.java
+++ src/com/deepu/dsa/recursion/SubSeq.java
@@ -1,10 +1,15 @@
 package com.deepu.dsa.recursion;
 
+import java.util.AbstractList;
+import java.util.ArrayList;
+
 public class SubSeq {
     static void main() {
-        subseq("","abc");
+//        subseq("","abc");
+        System.out.println(subseqRet("","abc"));
     }
 
+    // P -> processed, up -> unprocessed
     static void subseq(String p, String up){
         if(up.isEmpty()){
             System.out.println(p);
@@ -15,6 +20,22 @@ public class SubSeq {
 
         subseq(p+ch,up.substring(1));
         subseq(p,up.substring(1));
+    }
+
+    static ArrayList<String> subseqRet(String p, String up){
+        if(up.isEmpty()){  //  unprocessed string became null, just return processed string processed string
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);   // us substring ko add kar do list me
+            return list;
+        }
+
+        char ch = up.charAt(0);
+        ArrayList<String> left = subseqRet(p+ch,up.substring(1));
+        ArrayList<String> right = subseqRet(p,up.substring(1));
+
+        left.addAll(right);
+
+        return left;
     }
 
 }
