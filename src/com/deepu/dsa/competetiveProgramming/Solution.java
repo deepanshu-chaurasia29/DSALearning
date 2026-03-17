a387c7282e133e0270de81a2f29e15b4830e9ea2
Deepanshu Chaurasia <deepanshustm2003@gmail.com>
2026-03-18 04:11:30 +0530

    day 39 learnings

--- src/com/deepu/dsa/competetiveProgramming/Solution.java
+++ src/com/deepu/dsa/competetiveProgramming/Solution.java
@@ -1,12 +1,29 @@
 package com.deepu.dsa.competetiveProgramming;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Solution {
     public static void main(String[] args) {
-        int[] arr = {34,32,5,6,3,7,2,54};
+        // when you write namaste or hello or something else.
+        // java internally take it as characters or you can say unicode values
+        String english = "Hello";
+        String hindi = "नमस्ते";
+        String chinese = "你好";
+        String arabic = "مرحبا";
+        String russian = "Привет";
+        String japanese = "こんにちは";
+        String emoji = "😀🔥";
 
-        Arrays.sort(arr);
-        System.out.println(Arrays.toString(arr));
+        System.out.println("English: " + english);
+        System.out.println("Hindi: " + hindi);
+        System.out.println("Chinese: " + chinese);
+        System.out.println("Arabic: " + arabic);
+        System.out.println("Russian: " + russian);
+        System.out.println("Japanese: " + japanese);
+        System.out.println("Emoji: " + emoji);
+
+
     }
 }
