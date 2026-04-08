bed0ecdf3b24fb6cee7c7fda3a57c5b4b1414bd6
Deepanshu Chaurasia <deepanshustm2003@gmail.com>
2026-04-08 21:53:27 +0530

    day 60 learnings

---
+++ src/com/deepu/Main.java
@@ -0,0 +1,30 @@
+package com.deepu;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Main {
+    class myRuntimeException extends RuntimeException{
+
+        public myRuntimeException(String message){
+            super(message);
+        }
+    }
+    public static void main(String[] args) {
+        try {
+            File file = new File("data.txt");  // file name
+            Scanner sc = new Scanner(file);
+
+            while (sc.hasNextLine()) {
+                System.out.println(sc.nextLine());
+            }
+
+            sc.close();
+
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found!");
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+}
