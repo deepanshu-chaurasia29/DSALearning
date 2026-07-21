10e9d625d1f4a4a5b448923ec069b115d4866531
Deepanshu Chaurasia <deepanshustm2003@gmail.com>
2026-07-21 15:49:15 +0530

    Linked list me insert at index padha

--- src/com/deepu/dsa/LinkedList/LL.java
+++ src/com/deepu/dsa/LinkedList/LL.java
@@ -16,9 +16,13 @@ public class LL {
         public Node(int data){
             this.data = data;
         }
+        public Node(int data,Node next){
+            this.data=data;
+            this.next=next;
+        }
     }
 
-    // Insert an element in LL
+    // Insert at first position in LL
     public void insertFirst(int val){
         Node node=new Node(val);
         node.next=head;
@@ -30,31 +34,24 @@ public class LL {
         }
         size++;
     }
-    // Insert at last
+
+    // Insert at last position
     public void insertLast(int val){
+
+        if(head == null){
+            // means first element hai ye, so sidhe add kar do isko first position or
+            insertFirst(val);
+            return;
+        }
         Node node=new Node(val);
         tail.next=node;
         tail=node;
 
-        if(head==null){
-            head=tail;
-        }
+//        if(head==null){
+//            head=tail;
+//        }
         size++;
     }
-    //Delete
-
-
-
-
-
-
-    //Search
-
-
-
-
-
-
 
     // Print all elements of LL
     public void display(){
@@ -64,5 +61,44 @@ public class LL {
             temp=temp.next;
         }
         System.out.println("END");
+    }
+
+    // Insert in between
+    // to insert new element, we must know the previous and next element information.
+    public void insertInBetween(int value,int preVal, int nextVal){
+        Node node=new Node(value);
+        Node temp=head;
+        if(temp.data == preVal && temp.next.data == nextVal){
+            temp.next = node;
+            node.next = temp.next;
+        }
+        temp=temp.next;
+    }
+
+    public void insert(int val,int index){
+        if(index > size){
+            try {
+                throw new Exception("illegal addition");
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+        if(index == 0){
+            insertFirst(val);
+            return;
+        }
+        if(index == size){
+            insertLast(val);
+            return;
+        }
+        Node temp=head;
+        for(int i=1; i<index; i++){
+            temp = temp.next;
+        }
+        // when you come from this loop, you will point to previous node after after which new node is to be inserted
+
+        Node node=new Node(val,temp.next);
+        temp.next = node;
+        size++;
     }
 }
--- src/com/deepu/dsa/LinkedList/Main.java
+++ src/com/deepu/dsa/LinkedList/Main.java
@@ -3,12 +3,14 @@ package com.deepu.dsa.LinkedList;
 public class Main {
     static void main() {
         LL list=new LL();
-        list.insertFirst(5);
-        list.insertFirst(2);
-        list.insertFirst(3);
         list.insertFirst(1);
+        list.insertFirst(3);
+        list.insertFirst(4);
+        list.insertFirst(7);
+        list.insertFirst(8);
         list.display();
-        list.insertLast(8);
+        list.insert(5,3);
+//        list.insert(90,500);   exception occur
         list.display();
 
 
