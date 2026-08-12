4708888f307e21b574d3d81fa69fef98e99e1796
Deepanshu Chaurasia <deepanshustm2003@gmail.com>
2026-08-12 23:00:39 +0530

    stack and queue

---
+++ src/com/deepu/dsa/stack_queue/InBuiltExamples.java
@@ -0,0 +1,30 @@
+package com.deepu.dsa.stack_queue;
+
+import java.util.*;
+
+public class InBuiltExamples {
+    static void main() {
+//        Stack<Integer> stack=new Stack<>();
+//        stack.push(12);
+//        stack.push(13);
+//        stack.push(14);
+//
+//
+//        System.out.println(stack.pop());
+//        System.out.println(stack.pop());
+//        System.out.println(stack.pop());
+
+//        Queue<Integer> queue=new LinkedList<>();
+//        queue.add(1);
+//        queue.add(2);
+//        queue.add(3);
+//        queue.add(4);
+//
+//        System.out.println(queue.remove());
+
+
+        Deque<Integer> deque=new ArrayDeque<>();
+
+
+    }
+}
